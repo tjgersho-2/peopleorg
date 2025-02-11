a0b868052c3ce22d9b2840645952570105d0e459
tjgersho <tjgersho@gmail.com>
2025-02-11 12:53:10 -0700

    Es Lint rules update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["error"]
+    },
+  }),
 ];
 
 export default eslintConfig;
